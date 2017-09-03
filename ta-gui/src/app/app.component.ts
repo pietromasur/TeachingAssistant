e6169982cc9f14d1ee80dc0153b2a9777e7da4cd
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:23:24 -0300

    Resolvendo conflitos com o commit para evitar cpfs duplicados

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -16,9 +16,12 @@ export class AppComponent {
    alunos: Aluno[] = [];
 
    gravar(a: Aluno): void {
-     this.alunoService.gravar(a);
-     this.alunos.push(a);
-     this.aluno = {nome: "", cpf: "", email: "", login: ""};
+     if (this.alunoService.gravar(a)) {
+       this.alunos.push(a);
+       this.aluno = {nome: "", cpf: "", email: "", login: ""};
+     } else {
+       this.aluno.cpf = "";
+     }
   }
 
 }
